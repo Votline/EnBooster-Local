4ed9f6dbff906a949aceb172c5c2de7ae7890494
Votline <quogg2@gmail.com>
2026-07-27 13:08:09 +0300

    feat(frontend): create frontend direcatory and npm init

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <div class="chat-container">
+    <h1>Chat UI</h1>
+  </div>
+</template>
+
+<style scoped>
+.chat-container {
+  max-width: 600px;
+  margin: 0 auto;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: #0e1621;
+  color: #ffffff;
+  font-family: sans-serif;
+}
+</style>
