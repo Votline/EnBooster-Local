fd669df798e1588550dd5825f45db623f1398bc2
Votline <quogg2@gmail.com>
2026-08-01 12:28:56 +0300

    fix: deploy k8s script

--- deploy_k8s.sh
+++ deploy_k8s.sh
@@ -24,25 +24,33 @@ fi
 echo -e "${GREEN}==> 2. changed docker-cli to minikube...${NC}"
 eval $(minikube docker-env)
 
-echo -e "${GREEN}==> 3. building containers...${NC}"
+echo -e "${GREEN}==> 3. building containers locally...${NC}"
 docker build -t frontend:latest ./frontend
-docker build -t gateway:latest ./gateway
-docker build -t users-service:latest ./users-service
-docker build -t learn-service:latest ./learn-service
-docker build -t ai-service:latest ./ai-service
+docker build -t gateway:latest -f ./gateway/Dockerfile .
+docker build -t users-service:latest -f ./users-service/Dockerfile .
+docker build -t learn-service:latest -f ./learn-service/Dockerfile .
+docker build -t ai-service:latest -f ./ai-service/Dockerfile .
 
-echo -e "${GREEN}==> 4. applying kubernetes-manifests...${NC}"
+echo -e "${GREEN}==> 4. loading images into minikube...${NC}"
+minikube image load frontend:latest
+minikube image load gateway:latest
+minikube image load users-service:latest
+minikube image load learn-service:latest
+minikube image load ai-service:latest
+
+echo -e "${GREEN}==> 5. applying kubernetes-manifests...${NC}"
 kubectl apply -f k8s/
 
-echo -e "${GREEN}==> 5. waiting for gatewat & frontend...${NC}"
+echo -e "${GREEN}==> 6. waiting for gateway & frontend...${NC}"
 kubectl rollout status deployment/frontend --timeout=120s
 kubectl rollout status deployment/gateway --timeout=120s
 
-echo -e "${GREEN}==> 6. port-forwarding...${NC}"
+echo -e "${GREEN}==> 7. cleaning up old port-forwards...${NC}"
+pkill -f "kubectl port-forward" 2>/dev/null || true
+
+echo -e "${GREEN}==> 8. port-forwarding...${NC}"
 echo -e "${YELLOW}• frontend: http://localhost:5173${NC}"
 echo -e "${YELLOW}• gateway:  http://localhost:8080${NC}"
 
-kubectl port-forward svc/frontend 5173:5173 > /dev/null 2>&1 &
-kubectl port-forward svc/gateway 8080:8080 > /dev/null 2>&1 &
-
-wait
+kubectl port-forward svc/frontend 5173:5173 &
+kubectl port-forward svc/gateway 8080:8080 &
